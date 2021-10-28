8ef4cb359a522fa83605af390010b9b372635aff
Saiteja Alle <alle.saiteja@gmail.com>
2021-10-28 10:59:39 -0700

    Initial Commit

---
+++ SignUpForm/SignUpFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpFormApp.swift
+//  SignUpForm
+//
+//  Created by Saiteja Alle on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SignUpFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
